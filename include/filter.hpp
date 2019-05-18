e326a5b64fbaf232f2bda7f8fba46b0962c5ecc5
Hong-Wei Ng <lightalchemist@gmail.com>
2019-05-18 23:30:57 +0800

    Refactored code

--- include/filter.hpp
+++ include/filter.hpp
@@ -29,37 +29,33 @@ double kernel(const cv::Mat& I,
     double yr = I.at<double>(r1, c1);
     double ys = I.at<double>(r2, c2);
 
-    // Add in spatial term
     // double squareSpatialDist = (r1 - r2) * (r1 - r2) + (c1 - c2) * (c1 - c2);
     double squareIntensityDist = (yr - ys) * (yr - ys);
     // return std::exp(-spatialScale * squareSpatialDist - intensityScale * squareIntensityDist);
     return std::exp(- intensityScale * squareIntensityDist);
 }
 
-inline int to1DIndex(int row, int col, int ncols)
+inline
+int to1DIndex(int row, int col, int ncols)
 {
     return row * ncols + col;
 }
 
-inline std::pair<int, int> to2DCoords(int index, int ncols)
+inline
+std::pair<int, int> to2DCoords(int index, int ncols)
 {
     return std::make_pair(index / ncols, index % ncols);
 }
 
-double estimateVariance(const cv::Mat& I)
-{
-    return 100.0;
+void printNegativeEntries(const Eigen::MatrixXd& mat) {
+    for (int j = 0; j < mat.cols(); j++) {
+        for (int i = 0; i < mat.rows(); i++) {
+            if (mat(i, j) < 0) {
+                std::cout << i << ", " << j << " -ve val: " << mat(i, j) << std::endl;
+            }
+        }
+    }
 }
-
-// template <typename T>
-// std::vector<T> convertToVec(const cv::Mat& I)
-// {
-//     std::vector<T> v;
-//     v.reserve(I.total());
-//     std::copy(I.begin<T>(), I.end<T>(), std::back_inserter(v));
-//
-//     return v;
-// }
 
 auto samplePixels(int nrows, int ncols, int nRowSamples)
 {
@@ -103,29 +99,22 @@ computeKernelWeights(const cv::Mat& I,
 {
     int nrows = I.rows, ncols = I.cols;
     int nPixels = I.total();
-    assert(nPixels == nrows * ncols);
 
     std::vector<int> selected, rest;
     std::tie(selected, rest) = samplePixels(nrows, ncols, nRowSamples);
     int nSamples = selected.size();
-
-
     Ka.resize(nSamples, nSamples);
     Kab.resize(nSamples, nPixels - nSamples);
 
     std::cout << "Ka size: " << Ka.rows() << " x " << Ka.cols() << std::endl;
     std::cout << "Kab size: " << Kab.rows() << " x " << Kab.cols() << std::endl;
-
-    // TODO: Tune this
-    // double variance = estimateVariance(I);
-    // double variance = 1000;
     std::cout << "hy: " << hy << std::endl;
     double gammaIntensity = 1.0 / (hy * hy);
-    double gammaSpatial = 0; // 1.0 / 10;
+    double gammaSpatial = 0;
 
-    // Compute Ka
     int r1, c1, r2, c2;
     for (auto i = 0u; i < selected.size(); ++i) {
+        // Compute Ka
         std::tie(r1, c1) = to2DCoords(selected[i], ncols);
         for (auto j = i; j < selected.size(); ++j) {
             std::tie(r2, c2) = to2DCoords(selected[j], ncols);
@@ -134,32 +123,16 @@ computeKernelWeights(const cv::Mat& I,
             Ka(j, i) = val;
         }
 
+        // Compute Kab
         for (auto j = 0u; j < rest.size(); j++) {
             std::tie(r2, c2) = to2DCoords(rest[j], ncols);
             Kab(i, j) = kernel(I, r1, c1, r2, c2, gammaSpatial, gammaIntensity);
         }
     }
+    
+    std::cout << "Checking Ka for negative entries" << std::endl;
+    printNegativeEntries(Ka);
 
-    // for (auto i = 0u; i < selected.size(); ++i) {
-    //     std::tie(r1, c1) = to2DCoords(i, ncols);
-    //     for (auto j = 0u; j < selected.size(); ++j) {
-    //         std::tie(r2, c2) = to2DCoords(j, ncols);
-    //         Ka(i, j) = kernel(II, selected[i], selected[j],
-    //                           r1, c1, r2, c2,
-    //                           gammaSpatial, gammaIntensity);
-    //     }
-    // }
-
-    for (auto i = 0u; i < Ka.rows(); i++) {
-        for (auto j = 0u; j < Ka.cols(); j++) {
-            if (Ka(i, j) < 0) {
-                std::cout << "Ka(" << i << ", " << j << "): " << Ka(i, j) << std::endl;
-                throw std::runtime_error("Ka contains negative entries");
-            }
-        }
-    }
-
-    // Check that Ka is symmetric
     assert(Ka.isApprox(Ka.transpose()));
     if (Ka.isApprox(Ka.transpose())) {
         std::cout << "Ka is symmetric" << std::endl;
@@ -168,33 +141,15 @@ computeKernelWeights(const cv::Mat& I,
         std::cout << "Ka is NOT symmetric" << std::endl;
     }
 
-    // for (auto i = 0u; i < selected.size(); i++) {
-    //     std::tie(r1, c1) = to2DCoords(i, ncols);
-    //     for (auto j = 0u; j < rest.size(); j++) {
-    //         std::tie(r2, c2) = to2DCoords(j, ncols);
-    //         Kab(i, j) = kernel(II, selected[i], rest[j],
-    //                            r1, c1, r2, c2,
-    //                            gammaSpatial, gammaIntensity);
-    //     }
-    // }
-
-
     Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> P(nPixels);
-    int i = 0;
-    for (; i < selected.size(); ++i) {
+    for (int i = 0; i < selected.size(); ++i) {
         P.indices()[i] = selected[i];
     }
-    for (int j = 0; j < rest.size(); ++i, ++j) {
-        P.indices()[i] = rest[j];
+    for (int j = 0; j < rest.size(); ++j) {
+        P.indices()[j + selected.size()] = rest[j];
     }
 
     return P;
-
-    // pixelOrder.reserve(nPixels);
-    // pixelOrder.insert(pixelOrder.end(), selected.begin(), selected.end());
-    // pixelOrder.insert(pixelOrder.end(), rest.begin(), rest.end());
-
-
 }
 
 void reciprocal(Eigen::MatrixXd& mat, double eps=EPS) {
@@ -218,19 +173,14 @@ sinkhornKnopp(const Eigen::MatrixXd& phi,
     // TODO: Debug this by testing on small, almost symmetric, matrices.
 
     int n = phi.rows();
-    int k = phi.cols();
     Eigen::MatrixXd r = Eigen::VectorXd::Ones(n, 1);
-    Eigen::MatrixXd c; 
-
+    Eigen::MatrixXd c;
     Eigen::MatrixXd Dphi_t = eigvals.asDiagonal() * phi.transpose();
     for (int i = 0; i < maxIter; i++) {
-        // c = phi * (eigvals.array() * (phi.transpose() * r).array()).matrix();
         c = phi * (Dphi_t * r);
         reciprocal(c, eps);
-
         assert(c.rows() == phi.rows());
-
-        // r = phi * (eigvals.array() * (phi.transpose() * c).array()).matrix();
+        assert(c.cols() == 1);
         r = phi * (Dphi_t * c);
         reciprocal(r, eps);
     }
@@ -266,13 +216,10 @@ invertDiagMatrix(const Eigen::MatrixXd& mat, double eps = EPS)
         }
     }
 
-    // return std::make_pair(invMat.topLeftCorner(numNonZero, numNonZero).asDiagonal(),
-    //                       numNonZero);
-
     return std::make_pair(invMat.asDiagonal(), numNonZero);
 }
 
-auto eigenFactorize(const Eigen::MatrixXd& A, double eps=EPS) {
+auto eigenDecomposition(const Eigen::MatrixXd& A, double eps=EPS) {
     // Eigenfactorization of a gram matrix (PSD)
 
     // NOTE: right most vectors are the eigenvectors with largest eigenvalues
@@ -280,7 +227,13 @@ auto eigenFactorize(const Eigen::MatrixXd& A, double eps=EPS) {
     // es.compute(A);
     // Eigen::MatrixXd eigvals = es.eigenvalues();
     // Eigen::MatrixXd eigvecs = es.eigenvectors();
-
+    
+    // Eigendecomposition of Ka
+    // Eigen::EigenSolver<Eigen::MatrixXd> es(Ka);
+    // // TODO: Check this. This results in a conversion to MatrixXd?
+    // Eigen::MatrixXd eigvals = es.eigenvalues().real();
+    // Eigen::MatrixXd eigvecs = es.eigenvectors().real();
+    
     Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
 
     int rank = svd.rank();
@@ -316,22 +269,9 @@ auto eigenFactorize(const Eigen::MatrixXd& A, double eps=EPS) {
 auto nystromApproximation(const Eigen::MatrixXd& Ka, const Eigen::MatrixXd& Kab,
                           double eps = EPS)
 {
-
-    // Eigendecomposition of Ka
-    // Eigen::EigenSolver<Eigen::MatrixXd> es(Ka);
-    // // TODO: Check this. This results in a conversion to MatrixXd?
-    // Eigen::MatrixXd eigvals = es.eigenvalues().real();
-    // Eigen::MatrixXd eigvecs = es.eigenvectors().real();
-
-    // NOTE: right most vectors are the eigenvectors with largest eigenvalues
-    // Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es;
-    // es.compute(Ka);
-    // Eigen::MatrixXd eigvals = es.eigenvalues();
-    // Eigen::MatrixXd eigvecs = es.eigenvectors();
-
     Eigen::MatrixXd eigvals;
     Eigen::MatrixXd eigvecs;
-    std::tie(eigvecs, eigvals) = eigenFactorize(Ka);
+    std::tie(eigvecs, eigvals) = eigenDecomposition(Ka);
 
     // Approximate eigenvectors of K from the above eigenvalues and eigenvectors and Kab
     std::cout << "eigvals shape: " << eigvals.rows() << " x " << eigvals.cols() << std::endl;
@@ -364,27 +304,9 @@ void plotSampledPoints(cv::Mat& I, int nSamples)
     std::cout << "# selected: " << selected.size() << std::endl;
 }
 
-double getElement(const Eigen::VectorXd& v, int index) {
-    return v(index);
-}
-
-
 cv::Mat eigen2opencv(Eigen::VectorXd& v, int nrows, int ncols) {
     cv::Mat X(nrows, ncols, CV_64FC1, v.data());
     return X;
-}
-
-Eigen::VectorXd backwardPermute(const Eigen::VectorXd& v, std::vector<int>& order)
-{
-    Eigen::VectorXd sortedVec(v.size());
-
-    assert(v.size() == order.size());
-
-    for (int i = 0; i < v.size(); i++) {
-        sortedVec(order[i]) = v(i);
-    }
-
-    return sortedVec;
 }
 
 cv::Mat rescaleForVisualization(const cv::Mat& mat) {
@@ -404,7 +326,7 @@ auto orthogonalize(Eigen::MatrixXd& Wa, Eigen::MatrixXd& Wab, double eps=EPS) {
     // Eigen::MatrixXd eigvecs = es.eigenvectors().real();
 
     Eigen::MatrixXd eigvals, eigvecs;
-    std::tie(eigvecs, eigvals) = eigenFactorize(Wa, eps);
+    std::tie(eigvecs, eigvals) = eigenDecomposition(Wa, eps);
 
     // Eigen::MatrixXd invRootEigVals = eigvals.cwiseSqrt();
     Eigen::MatrixXd invRootEigVals = eigvals;
@@ -417,7 +339,7 @@ auto orthogonalize(Eigen::MatrixXd& Wa, Eigen::MatrixXd& Wab, double eps=EPS) {
     Eigen::MatrixXd Q = Wa + invRootWa * Wab * Wab.transpose() * invRootWa;
 
     Eigen::MatrixXd Sq, Vq;
-    std::tie(Vq, Sq) = eigenFactorize(Q, eps);
+    std::tie(Vq, Sq) = eigenDecomposition(Q, eps);
 
     // Eigen::MatrixXd invRootSq = Sq.cwiseSqrt();
     Eigen::MatrixXd invRootSq = Sq;
@@ -450,44 +372,27 @@ cv::Mat filterImage(const cv::Mat& I, std::vector<T>& weights, int nRowSamples, 
     std::vector<cv::Mat> channels;
     cv::split(Ilab, channels);
 
-    // TODO: Remember to convert back to 8U before merging
     // TODO: Check if value of L depends on original type.
     cv::Mat L = channels[0];
     L.convertTo(L, CV_64F);
 
-    for (int i = 0; i < L.rows; i++) {
-        for (int j = 0; j < L.cols; j++) {
-            if (L.at<double>(i, j) < 0) {
-                std::cout << "-ve L channel value: " << L.at<double>(i, j) << " at " << i << ", " << j << std::endl;
-            }
-        }
-    }
-
-
-    std::cout << "Computing kernel weights" << std::endl;
-    Eigen::MatrixXd Ka, Kab;
-
     cv::Mat tmpI = I.clone();
     plotSampledPoints(tmpI, nRowSamples);
     cv::imshow("sampled", tmpI);
-    // cv::waitKey(-1);
-    // return cv::Mat();
 
+    std::cout << "Computing kernel weights" << std::endl;
+    Eigen::MatrixXd Ka, Kab;
     Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> P = computeKernelWeights(L, Ka, Kab, nRowSamples, hy);
-
+    
+    std::cout << "Running Nystrom approximation" << std::endl;
     Eigen::MatrixXd eigvals, phi;
     std::tie(eigvals, phi) = nystromApproximation(Ka, Kab);
 
     std::cout << "Top 5 Eigenvalue: " << std::endl;
     std::cout << eigvals.topRows(5) << std::endl;
-    for (int j = 0; j < eigvals.cols(); j++) {
-        for (int i = 0; i < eigvals.rows(); i++) {
-            if (eigvals(i, j) < 0) {
-                std::cout << i << ", " << j << " -ve eigenvalue: " << eigvals(i, j) << std::endl;
-            }
-        }
-    }
+    printNegativeEntries(eigvals);
 
+    std::cout << "Running Sinkhorn Knopp algorithm." << std::endl;
     Eigen::MatrixXd Wa, Wab;
     std::tie(Wa, Wab) = sinkhornKnopp(phi, eigvals, 20);
     if (Wa.isApprox(Wa.transpose()), EPS) {
@@ -511,10 +416,8 @@ cv::Mat filterImage(const cv::Mat& I, std::vector<T>& weights, int nRowSamples, 
     Eigen::MatrixXd V, S;
     std::tie(V, S) = orthogonalize(Wa, Wab);
 
-    // std::cout << "S top k of total length: " << S.rows() << std::endl;
-    // std::cout << S.topRows(7) << std::endl;
-    std::cout << "Eigenvalues S" << std::endl;
-    std::cout << S << std::endl;
+     std::cout << "S top k of total length: " << S.rows() << std::endl;
+     std::cout << S.topRows(7) << std::endl;
 
     // Permute eigenvector entries back to the same order as the original image.
     V = P * V;
@@ -538,16 +441,14 @@ cv::Mat filterImage(const cv::Mat& I, std::vector<T>& weights, int nRowSamples, 
     }
 
     std::cout << "Row sum" << std::endl;
-    std::cout << (V.row(0) * S.asDiagonal() * V.transpose()).sum() << std::endl;
-    std::cout << (V.row(1) * S.asDiagonal() * V.transpose()).sum() << std::endl;
-    std::cout << (V.row(2) * S.asDiagonal() * V.transpose()).sum() << std::endl;
-    std::cout << (V.row(3) * S.asDiagonal() * V.transpose()).sum() << std::endl;
+    for (int i = 0; i < V.rows() && i < 4; ++i) {
+        std::cout << (V.row(i) * S.asDiagonal() * V.transpose()).sum() << std::endl;
+    }
 
     cv::waitKey(-1);
 
-    // cv::Mat Lv = L.reshape(0, V.rows());
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> lv;
-    lv.resize(V.rows(), 1);
+    lv.resize(L.total(), 1);
     int k = 0;
     for (int i = 0; i < L.rows; i++) {
         for (int j = 0; j < L.cols; j++) {
